274646ee6f1596b5fcfdbb013b5885aeb85a5693
Laurent VANDELLE <vandellelaurent@yahoo.fr>
2021-01-10 13:25:33 +0100

    before local serve

--- ui/src/app/shared/shared.module.ts
+++ ui/src/app/shared/shared.module.ts
@@ -3,8 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { TranslateModule } from '@ngx-translate/core';
 
-import { PageNotFoundComponent } from './components/';
-import { WebviewDirective } from './directives/';
+import { PageNotFoundComponent } from './components';
+import { WebviewDirective } from './directives';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
--- ui/src/app/view/analyse-board/analyse-board.module.ts
+++ ui/src/app/view/analyse-board/analyse-board.module.ts
@@ -1,6 +1,6 @@
-import { CoreModule } from './../../core/core.module';
-import { AppRoutingModule } from './../../app-routing.module';
-import { SharedModule } from './../../shared/shared.module';
+import { CoreModule } from '../../core/core.module';
+import { AppRoutingModule } from '../../app-routing.module';
+import { SharedModule } from '../../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AnalyseBoardComponent } from './pages/analyse-board/analyse-board.component';
