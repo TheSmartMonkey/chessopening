d4f05f0f09011d8fbe57d164f8a5758c464c9947
Laurent VANDELLE <vandellelaurent@yahoo.fr>
2021-01-16 21:07:57 +0100

    add modale form

--- app/src/js/opening-modal.js
+++ app/src/js/opening-modal.js
@@ -1,1 +1,6 @@
-
+$('#submit-form').on("click", function () {
+    const inputTitle = document.getElementById('title-input').value
+    const pieceColor = document.getElementById('piece-color').checked
+    console.log('TEST: ', inputTitle)
+    console.log('TEST: ', pieceColor)
+});
