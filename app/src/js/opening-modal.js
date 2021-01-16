ebe1ee53f5d2bd23d6870a08d0b2d4f8039ec825
Laurent VANDELLE <vandellelaurent@yahoo.fr>
2021-01-16 21:49:31 +0100

    create moves from PGN

--- app/src/js/opening-modal.js
+++ app/src/js/opening-modal.js
@@ -1,6 +1,21 @@
+function getFenFromPgn(pgn) {
+    const chess1 = new Chess();
+    const chess2 = new Chess();
+
+    chess1.load_pgn(pgn);
+    let moves = chess1.history().map(move => {
+        chess2.move(move);
+        return {'fen': chess2.fen()};
+    });
+
+    return moves
+}
+
 $('#submit-form').on("click", function () {
-    const inputTitle = document.getElementById('title-input').value
+    const titleInput = document.getElementById('title-input').value
     const pieceColor = document.getElementById('piece-color').checked
-    console.log('TEST: ', inputTitle)
+    const pgnInput = document.getElementById('pgn-input').value
+    console.log('TEST: ', titleInput)
     console.log('TEST: ', pieceColor)
+    console.log('TEST: ', getFenFromPgn(pgnInput));
 });
