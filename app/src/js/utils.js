e2dc45ef75e5a7b5252837c1ac6971cc4de65970
Laurent VANDELLE <vandellelaurent@yahoo.fr>
2022-02-05 20:10:17 +0100

    add opening file testing

--- app/src/js/utils.js
+++ app/src/js/utils.js
@@ -1,3 +1,6 @@
+const fs = require('fs')
+const path = require('path')
+
 export function getOpenings() {
     const filePath = path.resolve(__dirname, 'openings.json')
     const rawdata = fs.readFileSync(filePath)
